da7770d43a5dcb5c0f9f73ecbb902b9167402291
Doug Dunkle <ddunkle@contourmtg.com>
2020-09-23 15:37:16 -0400

    page coloring added, better load page function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {})
 
+var divIDS = ["hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"];
+var locStor = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+var curHour = parseInt(moment().hour())
+
 loadPage();
+colorPage();
+setTime();
+
 
 // Event listener for the save button click
 $(".saveBtn").on("click", function() {
@@ -11,13 +18,39 @@ $(".saveBtn").on("click", function() {
 
 //This will pull in the items from local storage
 function loadPage() {
-    $("#hour-9").children("textarea").text(localStorage.getItem("9AM"));
-    $("#hour-10").children("textarea").text(localStorage.getItem("10AM"));
-    $("#hour-11").children("textarea").text(localStorage.getItem("11AM"));
-    $("#hour-12").children("textarea").text(localStorage.getItem("12PM"));
-    $("#hour-13").children("textarea").text(localStorage.getItem("1PM"));
-    $("#hour-14").children("textarea").text(localStorage.getItem("2PM"));
-    $("#hour-15").children("textarea").text(localStorage.getItem("3PM"));
-    $("#hour-16").children("textarea").text(localStorage.getItem("4PM"));
-    $("#hour-17").children("textarea").text(localStorage.getItem("5PM"));
+    // Pulling items from local storage
+    // The arrays will go through each div tag
+    for (i=0; i<divIDS.length; i++) {
+        $("#" + divIDS[i]).children("textarea").text(localStorage.getItem(locStor[i]));
+    };
+    // Setting the current date
+    $("#currentDay").text(moment().format('ddd MMM Do YYYY'));    
+}
+
+function colorPage() {    
+    for (i=0; i<divIDS.length; i++) {
+        if (curHour > (i+9)) {
+            // add .past class
+            $("#" + divIDS[i]).children("textarea").addClass("past");
+        } else if (curHour === (i+9)) {
+            // add .present class
+            $("#" + divIDS[i]).children("textarea").addClass("present");
+        } else {
+            // add .future class
+            $("#" + divIDS[i]).children("textarea").addClass("future");
+        }
+    }
+
+}
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        if (curHour != parseInt(moment().hour())) {
+            curHour = parseInt(moment().hour())
+            colorPage();
+        };
+        
+        console.log(moment().minute());
+
+    }, 60000);
 }
